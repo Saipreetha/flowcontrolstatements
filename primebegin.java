364cb38e9bc914598f41030d307963695f2780bf
Saipreetha <saipreethaekb@gmail.com>
2017-06-30 12:36:31 +0530

    Add files via upload

---
+++ primebegin.java
@@ -0,0 +1,25 @@
+package guvitest;
+import java.util.Scanner;
+public class primebegin {
+	public static void main(String args[]){
+		System.out.print("Enter the starting number and ending number");
+		Scanner scan=new Scanner(System.in);
+		int input1=scan.nextInt();
+		int input2=scan.nextInt();
+		int flag;
+		scan.close();
+		for(int i=input1;i<=input2;i++){
+			flag=0;
+			 for(int j=2;j<i;j++){    
+				   if(i%j==0){       
+				   flag=1;        
+				   }
+				  }
+			 if(flag==0){
+				System.out.println(i);
+			 }
+			}  
+		}
+	}
+
+
